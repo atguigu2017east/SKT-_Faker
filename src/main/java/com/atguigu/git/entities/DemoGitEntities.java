3da5a3e0b2bef2717e054e49049c41c0b94a8f4f
Yanzh <Yanzh@atguigu.com>
2017-11-23 16:41:24 +0800

    this is faker!

--- src/main/java/com/atguigu/git/entities/DemoGitEntities.java
+++ src/main/java/com/atguigu/git/entities/DemoGitEntities.java
@@ -1,7 +1,6 @@
 package com.atguigu.git.entities;
 
 /**
- * 
  * @ClassName DemoGitEntities
  * @Description TODO(git demo 的实体类)
  * @author Yanzh
@@ -9,6 +8,77 @@ package com.atguigu.git.entities;
  * @version 1.0.0
  */
 public class DemoGitEntities {
+    
+    public static final Integer NUM_NO = 1;
 
     private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private String position;
+
+    private Integer honor;
+
+    public DemoGitEntities() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public DemoGitEntities(Integer id, String name, Integer age, String position, Integer honor) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.position = position;
+        this.honor = honor;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Integer getHonor() {
+        return honor;
+    }
+
+    public void setHonor(Integer honor) {
+        this.honor = honor;
+    }
+
+    @Override
+    public String toString() {
+        return "DemoGitEntities [id=" + id + ", name=" + name + ", age=" + age + ", position=" + position + ", honor="
+                + honor + "]";
+    }
+
 }
--- src/test/java/com/atguigu/git/test/DemoGitTest.java
+++ src/test/java/com/atguigu/git/test/DemoGitTest.java
@@ -1,5 +1,9 @@
 package com.atguigu.git.test;
 
+import org.junit.Test;
+
+import com.atguigu.git.entities.DemoGitEntities;
+
 /**
  * 
  * @ClassName DemoGitTest
@@ -10,8 +14,19 @@ package com.atguigu.git.test;
  */
 public class DemoGitTest {
     
+    @Test
     public void test() {
+        // 新建一个实体类对象
+        DemoGitEntities entity = new DemoGitEntities();
         
+        // 设置属性
+        entity.setId(DemoGitEntities.NUM_NO);
+        entity.setAge(21);
+        entity.setName("SKT1_Faker");
+        entity.setHonor(3);
+        
+        // 打印
+        System.out.println(entity);
     }
 
 }
