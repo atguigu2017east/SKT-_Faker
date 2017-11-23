97fb17f05280c8d7d39e407bd2ba3eeee1fee940
Yanzh <Yanzh@atguigu.com>
2017-11-23 15:57:41 +0800

    my fisrt commit by eclipse

---
+++ src/main/java/com/atguigu/git/entities/DemoGitEntities.java
@@ -0,0 +1,13 @@
+package com.atguigu.git.entities;
+
+/**
+ * 
+ * @ClassName DemoGitEntities
+ * @Description TODO(git demo 的实体类)
+ * @author Yanzh
+ * @Date 2017年11月23日 下午3:38:16
+ * @version 1.0.0
+ */
+public class DemoGitEntities {
+
+}
---
+++ src/test/java/com/atguigu/git/test/DemoGitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git.test;
+
+/**
+ * 
+ * @ClassName DemoGitTest
+ * @Description TODO(Git测试类)
+ * @author Yanzh
+ * @Date 2017年11月23日 下午3:39:26
+ * @version 1.0.0
+ */
+public class DemoGitTest {
+
+}
